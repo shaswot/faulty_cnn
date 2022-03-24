164472e504553c4e9dd3878bdb65a266ccefe13a
shaswot <shaswot@obcx>
2022-03-24 23:54:32 +0900

    added test_all script and verified in obcx

---
+++ obcx/test_all/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#PJM -g gq50
+#PJM -L rg=debug 
+#PJM -L node=1
+#PJM -L elapse=30:00
+#PJM -j
+
+module load singularity
+REPO=/work/gq50/q50002/faulty_cnn
+SIF=/work/gq50/q50002/faulty_cnn_ni.file
+
+cd ${REPO}
+
+singularity exec \
+    --bind `pwd` \
+    -H ${REPO} \
+    ${SIF} \
+    python ${REPO}/pyscripts/test_all.py
