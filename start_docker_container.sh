ceb54124532aab29cf75641915cfe9f619395d52
Shaswot Shresthamali <shaswot@titanium.acsl.ics.keio.ac.jp>
2022-05-19 16:34:57 +0900

    sync

--- start_docker_container.sh
+++ start_docker_container.sh
@@ -2,8 +2,8 @@
 docker run --gpus all \
         -dit \
         -v ~/stash:/stash \
-        --name container_name \
-        -p XXXX:8888 \
-        -p XXXX:6006 \
-	<docker_image_name> \
+        --name sunday \
+        -p 7750:8888 \
+        -p 7751:6006 \
+	bhootmali/faulty_cnn:san \
 	screen -S jlab jupyter lab --no-browser --ip=0.0.0.0 --port 8888 --allow-root --LabApp.token=''
