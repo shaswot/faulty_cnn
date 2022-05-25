8159be1d88a4db47aca73719635dee70bf3215af
sunday <san@titanium>
2022-05-25 04:13:49 +0000

    updated model seeds/error seeds generation script

--- libs/constants.py
+++ libs/constants.py
@@ -14,16 +14,14 @@ SEEDS_FOLDER = pathlib.Path(PROJ_ROOT_PATH / "seedfiles" )
 # load model seeds
 model_seeds_filename = "model_seeds.dat"
 model_seeds_file = pathlib.Path(SEEDS_FOLDER / model_seeds_filename)
-if model_seeds_file.is_file():
-    model_seeds = np.loadtxt(model_seeds_file, dtype=np.uintc).tolist()
-else:
+if not model_seeds_file.is_file():
     from seedfiles.seedgen import model_seeds, error_seeds
+model_seeds = np.loadtxt(model_seeds_file, dtype=np.uintc).tolist()
     
 
 # load error_profile seeds
 error_seeds_filename = "error_seeds.dat"
 error_seeds_file = pathlib.Path(SEEDS_FOLDER / error_seeds_filename)
-if error_seeds_file.is_file():
-    error_seeds = np.loadtxt(error_seeds_file, dtype=np.uintc).tolist()
-else:
+if not error_seeds_file.is_file():
     from seedfiles.seedgen import model_seeds, error_seeds
+error_seeds = np.loadtxt(error_seeds_file, dtype=np.uintc).tolist()
