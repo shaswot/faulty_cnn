477d948a5ce9419a93fe281870b3a766e92f3e2d
sunday <san@titanium>
2022-06-02 07:40:33 +0000

    wisteria script for c0_h2-ERR1_ERR-1

---
+++ wisteria_scripts/all/mnist32-cnn/c0_h2--ERR1_ERR-1/pydispatch.py
@@ -0,0 +1,14 @@
+import subprocess
+
+experiment = "mnist32-cnn_1024_256_64-1023--LIM_01"
+model_instance, error_lim = experiment.split("--")
+
+exp_script_list = [ 
+                    "all-mnist32-cnn--c0_h2--ERR1_ERR-1",
+                  ]
+
+for script_name in exp_script_list:
+    cmd_script = "exp_name="+script_name + " model_instance="+model_instance + " error_lim="+error_lim +" bash dispatch.sh"
+    print(cmd_script)
+    subprocess.call(cmd_script, shell=True)
+    
