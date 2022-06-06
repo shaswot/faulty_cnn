cfd5568b8dcca0278e71aa2f17a46b7181b10502
sunday <san@titanium>
2022-06-06 01:51:07 +0000

    added fashion-cnn2 ga scripts + wisteria scripts

---
+++ wisteria_scripts/all/fashion-cnn2/c0_ERR_-1/pydispatch.py
@@ -0,0 +1,14 @@
+import subprocess
+
+experiment = "fashion-cnn2_1024-1023--LIM_500"
+model_instance, error_lim = experiment.split("--")
+
+exp_script_list = [ 
+                    "all-fashion-cnn2--c0--ERR_-1",
+                  ]
+
+for script_name in exp_script_list:
+    cmd_script = "exp_name="+script_name + " model_instance="+model_instance + " error_lim="+error_lim +" bash dispatch.sh"
+    print(cmd_script)
+    subprocess.call(cmd_script, shell=True)
+    
