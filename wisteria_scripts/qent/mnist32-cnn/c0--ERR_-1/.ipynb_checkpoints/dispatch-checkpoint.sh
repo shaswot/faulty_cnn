8d7e6a55770262791244c19c9ea86d148230dc03
sunday <san@titanium>
2022-06-08 02:22:31 +0000

    fixed and added ga + wisteria scripts for qent

--- wisteria_scripts/qent/mnist32-cnn/c0--ERR_-1/.ipynb_checkpoints/dispatch-checkpoint.sh
+++ wisteria_scripts/qent/mnist32-cnn/c0--ERR_-1/.ipynb_checkpoints/dispatch-checkpoint.sh
@@ -35,15 +35,7 @@ singularity exec \
 	--bind `pwd` \
 	-H /work/gq50/q50002/faulty_cnn\
 	/work/gq50/q50002/faulty_cnn_san.file \
-	python ${exp_name}.py ${model_instance} ${error_lim}-4981 > ${log_folder}/${model_instance}--${error_lim}-4981".log" 2>&1 &
-
-
-singularity exec \
-	--bind `pwd` \
-	-H /work/gq50/q50002/faulty_cnn\
-	/work/gq50/q50002/faulty_cnn_san.file \
-    python ${exp_name}.py ${model_instance} ${error_lim}-3987 > ${log_folder}/${model_instance}--${error_lim}-3987".log" 2>&1
-
+	python ${exp_name}.py ${model_instance} ${error_lim}-4981 > ${log_folder}/${model_instance}--${error_lim}-4981".log" 2>&1
 
 
 EOF
